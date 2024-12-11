a8d1875f6a9f6c5d4ab387a046fa6ed846e05cb6
Husanboy Zafarov <zhzr1222@gmail.com>
2024-12-11 15:22:49 +0500

    Install: Reqct Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
-import App from "./App.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>
 );
---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,25 @@
+import useTodos from "./hooks/useTodos";
+
+const TodoList = () => {
+  const { data: todos, error, isLoading } = useTodos();
+
+  if (isLoading)
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todos?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.id} | {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
